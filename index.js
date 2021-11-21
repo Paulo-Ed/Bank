0ebe0acb4a5af80c1e313e303b1a27369b1ebcc6
Paulo Santos <pauloed_@outlook.com>
2021-11-21 14:40:59 -0300

    Criando a classe Conta

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 class Cliente{
-    constructor(nome, cpf, agencia, saldo){
+    constructor(nome, cpf, agencia, conta){
         this.nome = nome
         this.cpf = cpf
         this.agencia = agencia
-        this.saldo = saldo
+        this.conta = conta
     }
     
 }
 
+class Conta{
+    constructor(agencia, numero, saldo){
+        this.agencia = agencia
+        this.numero = numero
+        this._saldo = saldo
+    }
 
+    saque(valor){
+        this._saldo -= valor
+        return console.log(`Transação Aprovada!\nValor sacado: R$ ${valor}`)
+    }
 
+    deposito(valor){
+        this._saldo += valor
+        return console.log(`Valor Depositado: R$ ${valor} \nSaldo: R$ ${this._saldo}`)
+    }
+
+    saldo(){
+        return console.log(`Saldo: R$ ${this._saldo}`)
+    }
+}
+
+//Criando novo Cliente
 const cliente1 = new Cliente("Paulo", 12345678901, 0001, 0)
-
 console.log(cliente1.nome)
+
+//Criando Conta
+const conta1 = new Conta(0001, "12345-6", 0)
+conta1.deposito(200)
+conta1.saldo()
+conta1.saque(150)
+conta1.saldo()
