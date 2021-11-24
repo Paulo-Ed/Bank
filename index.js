c0f36100e216a79a441eb618af707c7e5da818b7
Paulo Santos <pauloed_@outlook.com>
2021-11-24 20:37:33 -0300

    Criando novos objetos e os manipulando de diferentes maneiras

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ const conta2 = new Conta(1123, "3457-9", cliente2, 500)
 conta1.saldo()
 conta2.saldo()
 conta2.transfere(200, conta1)
+
+//Criando Conta3 e definindo a partir dela um cliente associado
+const conta3 = new Conta(1123, "45678-2")
+conta3.cliente = new Cliente("Santos", 23415687907)
+console.log(conta3)
+
+//Criando Conta4 e o associando a um novo cliente dinâmicamente
+const conta4 = new Conta(1123, "55933-1")
+conta4.cliente = new Cliente()
+conta4.cliente.nome = "Cleide"
+//conta4.cliente.cpf = 33456711198
+console.log(conta4)
+
+//Acessando atributos de classe
+console.log(Conta.totalContas)
