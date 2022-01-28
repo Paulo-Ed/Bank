0f9e4cc47323bedc156afcec3c6cf66e36b9a4b2
Paulo Santos <pauloed_@outlook.com>
2022-01-28 14:03:10 -0300

    ajuste de autenticação

--- Autenticacao.js
+++ Autenticacao.js
@@ -1,6 +1,13 @@
 
 export class Autenticacao {
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha)
+        if(Autenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha)
+        }
+        return false
+    }
+
+    static ehAutenticavel(autenticavel) {
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
